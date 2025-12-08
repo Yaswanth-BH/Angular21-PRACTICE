c9e0fc3224d910f2af1c0d8bc928ecfe1894d074
Yaswanth-BH <bharanikapulayaswanthofficial@gmail.com>
2025-12-08 14:26:56 +0530

    Episode-05:Dynamic Css

---
+++ src/app/components/dynamic-css-class/dynamic-css-class.ts
@@ -0,0 +1,29 @@
+import { NgClass, NgStyle } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-dynamic-css-class',
+  imports: [NgClass, FormsModule, NgStyle],
+  templateUrl: './dynamic-css-class.html',
+  styleUrl: './dynamic-css-class.css',
+})
+export class DynamicCssClass {
+  myClassName: string = 'bg-warning';
+  isActive: boolean = false;
+  productPrice = 500;
+  divBackColor: string = '';
+  isSidePanel = signal(false);
+
+  myCss = {
+    color: 'red',
+    'background-color': 'black',
+    'font-size': '35px',
+  };
+  openSidePanel() {
+    this.isSidePanel.set(true);
+  }
+  closeSidePanel() {
+    this.isSidePanel.set(false);
+  }
+}
