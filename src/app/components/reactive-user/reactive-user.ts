c19dc68c71107982ae0dbf147ec046b1367367ca
Yaswanth-BH <bharanikapulayaswanthofficial@gmail.com>
2025-12-09 19:48:52 +0530

    Episode-06:Template, Reactive form and Validator

---
+++ src/app/components/reactive-user/reactive-user.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-reactive-user',
+  imports: [ReactiveFormsModule],
+  templateUrl: './reactive-user.html',
+  styleUrl: './reactive-user.css',
+})
+export class ReactiveUser {
+  userForm: FormGroup = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(5)]),
+    userName: new FormControl('', [Validators.required]),
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
+}
